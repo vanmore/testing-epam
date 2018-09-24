21a7dd625021f8aabfa941bc5d52d6e913b5a0c2
Ivan Kudryavtsev <van.kudr.mob@gmail.com>
2018-09-24 22:12:05 +0300

    Add comments to main

--- git/src/com/vankudr/Main.java
+++ git/src/com/vankudr/Main.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
-
+        //create group 9
         StudyGroup group9 = new StudyGroup("9");
 
+        //populating created group with fake students
         FakeGroupPopulator fakeGroupPopulator = new FakeGroupPopulator();
         fakeGroupPopulator.populate(group9);
 
+        //print marks and avearage marks for every student and for group
         System.out.println(group9.toString());
-
     }
 }
