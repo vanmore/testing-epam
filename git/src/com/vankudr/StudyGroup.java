7d404c9db891a578ad48a432b96def9de23144ce
Ivan Kudryavtsev <van.kudr.mob@gmail.com>
2018-09-24 23:09:21 +0300

    Revert "Rename a parameter in addStudent method"
    This reverts commit d7d65eff62708dcf8230bcdf84ee49695871ceb6.

--- git/src/com/vankudr/StudyGroup.java
+++ git/src/com/vankudr/StudyGroup.java
@@ -16,8 +16,8 @@ public class StudyGroup {
     }
 
 
-    public void addStudent(Student student) {
-        this.students.add(student);
+    public void addStudent(Student studentToAdd) {
+        this.students.add(studentToAdd);
     }
 
     public StudyGroup(String groupName) {
